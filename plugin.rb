cb55b266fda3918ba66bca3efbfd8894a3f6e6c3
jcpaiva <josepaiva94@gmail.com>
2026-05-12 18:47:45 +0100

    fix: restore mandatory plugin header

--- plugin.rb
+++ plugin.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+# name: discourse-saml
+# about: SAML Auth Provider
+# version: 1.1
+# authors: Discourse, INOV
+# url: https://github.com/UMinho-FA-AP/discourse-saml
+
 # Diagnostic patch to find out what is nil during activation
 class Plugin::Instance
   unless method_defined?(:original_activate!)
@@ -13,10 +20,6 @@ class Plugin::Instance
   end
 end
 
-# Discourse SAML Plugin for AMA (Autenticacao.gov)
-# Version: 1.1
-
-# 1. Global Patch (defined but only active via Thread-Local)
 after_initialize do
   require "onelogin/ruby-saml/authrequest"
   require "omniauth-saml"
@@ -27,23 +30,18 @@ after_initialize do
 
       def create_xml_doc(settings, params = {})
         doc = original_create_xml_doc(settings, params)
-
         if Thread.current[:ama_saml_patch_enabled]
           puts "AMA: Global create_xml_doc patch EXECUTING!"
-          
           fa_ns = "http://autenticacao.cartaodecidadao.pt/atributos"
           root = doc.root
           extensions = root.elements["samlp:Extensions"] || root.add_element("samlp:Extensions")
-          
           if root.elements["saml:Issuer"] && extensions.parent == root
             issuer = root.elements["saml:Issuer"]
             root.delete_element(extensions)
             root.insert_after(issuer, extensions)
           end
-
           level = Thread.current[:ama_faaalevel] || "3"
           extensions.add_element("fa:FAAALevel", { "xmlns:fa" => fa_ns }).text = level.to_s
-          
           req_attrs = extensions.add_element("fa:RequestedAttributes", { "xmlns:fa" => fa_ns })
           (Thread.current[:ama_requested_attributes] || "").split("|").map(&:strip).each do |attr_name|
             next if attr_name.empty?
@@ -59,18 +57,14 @@ after_initialize do
     end
   end
 
-  # Load other components
-  require_relative "lib/ama_authnrequest_extension"
   require_relative "lib/discourse_saml/saml_omniauth_strategy"
-  require_relative "lib/discourse_saml/saml_replay_cache"
+  require_relative "lib/saml_authenticator"
   
   puts "AMA: SamlAuthenticator components loaded"
 end
 
-# 2. Authenticator registration (Top Level)
 require_relative "lib/saml_authenticator"
 
-# Use ENV directly to ensure settings are available during the build phase
 title = ENV["DISCOURSE_SAML_TITLE"] || "SAML"
 button_title = ENV["DISCOURSE_SAML_BUTTON_TITLE"] || title
 
